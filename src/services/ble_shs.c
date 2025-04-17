7791c23c0a75477b694429455c24e18582a9dd6e
Collin-Pol <collin.ohm@polaris.com>
2025-04-17 13:41:17 -0500

    Update ble_shs.c

--- src/services/ble_shs.c
+++ src/services/ble_shs.c
@@ -22,8 +22,7 @@
 LOG_MODULE_REGISTER(ble_shs);
 
 #define BT_UUID_SENSOR_HUB              BT_UUID_DECLARE_128(SENSOR_HUB_SERVICE_UUID)
-#define BT_UUID_SENSOR_HUB_TEMP         BT_UUID_DECLARE_128(TEMP_CHARACTERISTIC_UUID)
-#define BT_UUID_SENSOR_HUB_HUMIDITY     BT_UUID_DECLARE_128(HUMIDITY_CHARACTERISTIC_UUID)
+#define BT_UUID_SENSOR_HUB_PRESSURE     BT_UUID_DECLARE_128(PRESSURE_CHARACTERISTIC_UUID)
 
 /*This function is called whenever the Client Characteristic Control Descriptor (CCCD) has been 
 changed by the GATT client, for each of the characteristics*/
@@ -50,13 +49,7 @@ static void on_cccd_changed(const struct bt_gatt_attr *attr, uint16_t value)
 BT_GATT_SERVICE_DEFINE(sensor_hub,
     BT_GATT_PRIMARY_SERVICE(BT_UUID_SENSOR_HUB),
     
-    BT_GATT_CHARACTERISTIC(BT_UUID_SENSOR_HUB_TEMP,
-                    BT_GATT_CHRC_NOTIFY,
-                    BT_GATT_PERM_READ,
-                    NULL, NULL, NULL),
-    BT_GATT_CCC(on_cccd_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
-    
-    BT_GATT_CHARACTERISTIC(BT_UUID_SENSOR_HUB_HUMIDITY,
+    BT_GATT_CHARACTERISTIC(BT_UUID_SENSOR_HUB_PRESSURE,
                     BT_GATT_CHRC_NOTIFY,
                     BT_GATT_PERM_READ,
                     NULL, NULL, NULL),
@@ -65,13 +58,13 @@ BT_GATT_SERVICE_DEFINE(sensor_hub,
 
 /* The below functions send a notification to a GATT client with the provided data,
 given that the CCCD has been set to Notify (0x1) */
-void sensor_hub_update_temperature(struct bt_conn *conn, const uint8_t *data, uint16_t len)
+void sensor_hub_update_pressure(struct bt_conn *conn, const uint8_t *data, uint16_t len)
 {
     const struct bt_gatt_attr *attr = &sensor_hub.attrs[2]; 
 
     struct bt_gatt_notify_params params = 
     {
-        .uuid   = BT_UUID_SENSOR_HUB_TEMP,
+        .uuid   = BT_UUID_SENSOR_HUB_PRESSURE,
         .attr   = attr,
         .data   = data,
         .len    = len,
@@ -81,40 +74,13 @@ void sensor_hub_update_temperature(struct bt_conn *conn, const uint8_t *data, ui
     if(bt_gatt_is_subscribed(conn, attr, BT_GATT_CCC_NOTIFY)) 
     {
         // Send the notification
-	    if(bt_gatt_notify_cb(conn, &params))
+    if(bt_gatt_notify_cb(conn, &params))
         {
             LOG_ERR("Error, unable to send notification");
         }
     }
     else
     {
-        LOG_WRN("Warning, notification not enabled for temperature characteristic");
-    }
-}
-
-void sensor_hub_update_humidity(struct bt_conn *conn, const uint8_t *data, uint16_t len)
-{
-    const struct bt_gatt_attr *attr = &sensor_hub.attrs[5]; 
-
-    struct bt_gatt_notify_params params = 
-    {
-        .uuid   = BT_UUID_SENSOR_HUB_HUMIDITY,
-        .attr   = attr,
-        .data   = data,
-        .len    = len,
-        .func   = NULL
-    };
-
-    if(bt_gatt_is_subscribed(conn, attr, BT_GATT_CCC_NOTIFY)) 
-    {
-        // Send the notification
-	    if(bt_gatt_notify_cb(conn, &params))
-        {
-            LOG_ERR("Error, unable to send notification");
-        }
-    }
-    else
-    {
-        LOG_WRN("Warning, notification not enabled for humidity characteristic");
+        LOG_WRN("Warning, notification not enabled for pressure characteristic");
     }
 }
