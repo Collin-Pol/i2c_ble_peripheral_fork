775be0cb087cc970fdc26e6c34a395553230d3a0
Collin-Pol <collin.ohm@polaris.com>
2025-04-24 09:35:07 -0500

    Update lps22.c

--- src/lps22.c
+++ src/lps22.c
@@ -1,3 +1,4 @@
+
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/sys/util.h>
@@ -21,60 +22,99 @@ const struct device *i2c_device = DEVICE_DT_GET(DT_NODELABEL(i2c22));
 
 bool lps22_init(void)
 {
-    int err;
-    if (!device_is_ready(dev_i2c.bus))
-    {
-        printk("I2C bus %s is not ready!", dev_i2c.bus->name);
-        return false;
-    }
+    int err;
+    if (!device_is_ready(dev_i2c.bus))
+    {
+        printk("I2C bus %s is not ready!", dev_i2c.bus->name);
+        return false;
+    }
 
-    uint8_t cmd[] = {LPS22_RESET_CMD};
-    err = i2c_write_dt(&dev_i2c, cmd, sizeof(cmd));
-    if (err != 0)
-    {
-        LOG_WRN("Failed to write to i2c dev addr");
-        return false;
-    }
+    uint8_t cmd[] = {LPS22_RESET_CMD};
+    err = i2c_write_dt(&dev_i2c, cmd, sizeof(cmd));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write to i2c dev addr");
+        return false;
+    }
 
-    // power management
-    err = pm_device_action_run(i2c_device, PM_DEVICE_ACTION_SUSPEND);
+    // power management
+    err = pm_device_action_run(i2c_device, PM_DEVICE_ACTION_SUSPEND);
 
-    return true;
+    return true;
 }
 
-float lps22_get_pressure(void)
+float lps22_read_pressure(void)
 {
-    int err;
-    float pressure;
+    int err;
+    float pressure;
 
-    err = pm_device_action_run(i2c_device, PM_DEVICE_ACTION_RESUME);
+    err = pm_device_action_run(i2c_device, PM_DEVICE_ACTION_RESUME);
 
-    if (!device_is_ready(dev_i2c.bus))
-    {
-        LOG_WRN("I2C bus %s is not ready!", dev_i2c.bus->name);
-        return 0;
-    }
+    if (!device_is_ready(dev_i2c.bus))
+    {
+        LOG_WRN("I2C bus %s is not ready!", dev_i2c.bus->name);
+        return 0;
+    }
 
-    // start conversion
-    uint8_t cmd[] = {LPS22_MEAS_PRESSURE_CMD};
-    uint8_t buf[2] = {0};
-    err = i2c_write_dt(&dev_i2c, cmd, sizeof(cmd));
-    if (err != 0)
-    {
-        LOG_WRN("Failed to write/read to i2c dev");
-        return LPS22_ERRCODE;
-    }
-    k_msleep(20); // conversion time
+    // start conversion
+    uint8_t cmd[] = {LPS22_PRESS_OUT_XL};
+    uint8_t buf[3] = {0};
+    err = i2c_write_dt(&dev_i2c, cmd, sizeof(cmd));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write/read to i2c dev");
+        return LPS22_ERRCODE;
+    }
+    k_msleep(20); // conversion time
 
-    err = i2c_read_dt(&dev_i2c, buf, sizeof(buf));
-    if (err != 0)
-    {
-        LOG_WRN("Failed to write/read to i2c dev");
-        return LPS22_ERRCODE;
-    }
-    
-    err = pm_device_action_run(i2c_device, PM_DEVICE_ACTION_SUSPEND);
+    err = i2c_read_dt(&dev_i2c, buf, sizeof(buf));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write/read to i2c dev");
+        return LPS22_ERRCODE;
+    }
 
-    pressure = (buf[0] << 8) | buf[1];
-    return pressure;
+    err = pm_device_action_run(i2c_device, PM_DEVICE_ACTION_SUSPEND);
+
+    pressure = ((buf[2] << 16) | (buf[1] << 8) | buf[0]) / 4096.0;
+    return pressure;
+}
+
+float lps22_read_temperature(void)
+{
+    int err;
+    float temperature_C;
+
+    err = pm_device_action_run(i2c_device, PM_DEVICE_ACTION_RESUME);
+
+    if (!device_is_ready(dev_i2c.bus))
+    {
+        printk("I2C bus %s is not ready!", dev_i2c.bus->name);
+        return 0;
+    }
+
+    // start conversion
+    uint8_t cmd[] = {LPS22_TEMP_OUT_L};
+    uint8_t buf[2] = {0};
+    err = i2c_write_dt(&dev_i2c, cmd, sizeof(cmd));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write/read to i2c dev");
+        return LPS22_ERRCODE;
+    }
+
+    k_msleep(20); // conversion time
+
+    err = i2c_read_dt(&dev_i2c, buf, sizeof(buf));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write/read to i2c dev");
+        return LPS22_ERRCODE;
+    }
+
+    err = pm_device_action_run(i2c_device, PM_DEVICE_ACTION_SUSPEND);
+
+    // unit conversion
+    temperature_C = ((buf[1] << 8) | buf[0]) / 100.0;
+    return temperature_C;
 }
