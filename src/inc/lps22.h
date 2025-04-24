ad45c7d470a5bc188744b03626d980784ed90451
Collin-Pol <collin.ohm@polaris.com>
2025-04-24 09:31:33 -0500

    Update lps22.h

--- src/inc/lps22.h
+++ src/inc/lps22.h
@@ -1,36 +1,37 @@
+
 #ifndef LPS22_H_
 #define LPS22_H_
 
 #define LPS22_I2C_TIMEOUT 100 // 100ms timeout
 
 /*!
- *  I2C ADDRESS/BITS
+ *  I2C ADDRESS/BITS
  */
 #define LPS22_DEFAULT_ADDRESS 0x5C
 
-#define LPS22_MEAS_PRESSURE_CMD \
-    0x21 /**< Measure Pressure Command */
-#define LPS22_RESET_CMD 0x11 /**< Reset Command */
+#define LPS22_WHO_AM_I_REG 0x0F /**< Who am I register */
+#define LPS22_CTRL_REG1 0x10    /**< Control register 1 */
+#define LPS22_CTRL_REG2 0x11    /**< Control register 2 */
+#define LPS22_STATUS_REG 0x27   /**< Status register */
+#define LPS22_PRESS_OUT_XL 0x28 /**< Pressure output LSB */
+#define LPS22_PRESS_OUT_L 0x29  /**< Pressure output middle byte */
+#define LPS22_PRESS_OUT_H 0x2A  /**< Pressure output MSB */
+#define LPS22_TEMP_OUT_L 0x2B   /**< Temperature output LSB */
+#define LPS22_TEMP_OUT_H 0x2C   /**< Temperature output MSB */
+#define LPS22_RESET_CMD 0x21    /**< Reset command */
 
 #define LPS22_ERRCODE 0xBADC0DE
 
 struct lps22_reading
 {
-    float pressure;
+    float pressure;
+    float temperature;
 };
 
-/* i2c dt fxns
-i2c_write_dt(...)
-i2c_read_dt(...)
-i2c_burst_read_dt(...)
-i2c_write_read_dt(...)
-*/
-
+/* I2C functions */
 bool lps22_init(void);
-float lps22_get_pressure(void);
+float lps22_read_pressure(void);
+float lps22_read_temperature(void);
 
-//! TODO: should totally implement these and stuff.
-void lps22_read_revision(void);
-void lps22_read_serial_num(void);
+#endif
 
-#endif // LPS22_H_
